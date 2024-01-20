3b7984d7cc80e575a1694c562403485f96973c6f
devhumayun <humayunkabir9408@gmail.com>
2024-01-20 22:48:54 +0600

    Import Sidebar and footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./component/footer/Footer";
 import Header from "./component/header/Header";
+import SideBar from "./component/sidebar/SideBar";
 
 export default function App() {
   return (
     <>
       <Header />
+      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <SideBar />
+      </div>
+      <Footer />
     </>
   )
 }
